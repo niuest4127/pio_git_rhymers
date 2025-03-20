8eba825a3a0aaaaf744b5d813fcf0a91dbec70b8
niuest4127 <251125@edu.p.lodz.pl>
2025-03-20 17:30:23 +0100

    poprawa konwencji nazewniczej kodu

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,21 +1,21 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FifoRhymer extends DefaultCountingOutRhymer {
 
-    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
-    
+    private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+
     @Override
     public int countOut() {
-        while (!callCheck())
-            
-        temp.countIn(super.countOut());
-        
-        int ret = temp.countOut();
-        
-        while (!temp.callCheck())
-            
-        countIn(temp.countOut());
-        
-        return ret;
+        while (!callCheck()) {
+            temp.countIn(super.countOut());
+        }
+
+        int result = temp.countOut();
+
+        while (!temp.callCheck()) {
+            countIn(temp.countOut());
+        }
+
+        return result;
     }
 }
