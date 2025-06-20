9d2350ccf4adba1fe970fa37dd14b78dc6999d8a
niuest4127 <251125@edu.p.lodz.pl>
2025-06-20 15:10:31 +0200

    Update RhymersDemo.java - task 13 zrobiony

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -4,30 +4,39 @@ import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.RhymersFactory;
 
 class RhymersDemo {
-
-    public static void main(String[] args) {
-        RhymersFactory factory = new DefaultRhymersFactory();
+    
+    public static void main(final String[] args) {
+        final RhymersFactory factory = new DefaultRhymersFactory();
+        testRhymers(factory);
+    }
+    
+    public static void testRhymers(final RhymersFactory factory) {
+        final DefaultCountingOutRhymer[] rhymers = { 
+            factory.getStandardRhymer(), 
+            factory.getFalseRhymer(),
+            factory.getFIFORhymer(), 
+            factory.getHanoiRhymer()
+        };
         
-        DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
-                factory.getFIFORhymer(), factory.getHanoiRhymer()};
-        
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = 1; i < 15; i++) {
+            for (int j = 0; j < 3; j++) {
                 rhymers[j].countIn(i);
+            }
+        }
         
-        java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < 15; i++)
+        final java.util.Random rn = new java.util.Random();
+        for (int i = 1; i < 15; i++) {
             rhymers[3].countIn(rn.nextInt(20));
+        }
         
         for (int i = 0; i < rhymers.length; i++) {
-            while (!rhymers[i].callCheck())
+            while (!rhymers[i].callCheck()) {
                 System.out.print(rhymers[i].countOut() + "  ");
+            }
             System.out.println();
         }
         
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).reportRejected());
-        
     }
-    
 }
