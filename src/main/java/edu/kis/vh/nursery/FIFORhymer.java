2011772147e0941899bb6707a892eca4fe991102
niuest4127 <251125@edu.p.lodz.pl>
2025-06-20 15:02:15 +0200

    Update FIFORhymer.java - zmiana na final temp

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 private class FIFORhymer extends DefaultCountingOutRhymer {
     
-    public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+    private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
     
     @Override
     public int countOut() {
