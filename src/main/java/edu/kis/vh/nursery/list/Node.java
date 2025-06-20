721704fd647c1657a6d3f88b780984cc447a894d
niuest4127 <251125@edu.p.lodz.pl>
2025-06-20 15:08:23 +0200

    Update Node.java - dodanie final oraz zrobiony punkt 11

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,13 +2,33 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
     
-    public int value;
-    public Node prev;
-    public Node next;
+    private final int value;
+    private Node prev;
+    private Node next;
     
-    public Node(int i) {
+    public Node(final int i) {
         value = i;
         prev = null;
         next = null;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+    
+    public Node getPrev() {
+        return prev;
+    }
+    
+    public void setPrev(final Node prev) {
+        this.prev = prev;
+    }
+    
+    public Node getNext() {
+        return next;
+    }
+    
+    public void setNext(final Node next) {
+        this.next = next;
     }
 }
